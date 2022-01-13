d4466e3e015181ef12f39d82982621c74c856828
Josh Siva <jtsiva@gmail.com>
2022-01-12 20:27:57 -0800

    added function to get unique characters in orthographies

--- tools/ortho-diff.py
+++ tools/ortho-diff.py
@@ -3,22 +3,46 @@
 import argparse
 import json
 
-if __name__ == "__main__":
+def get_unique_chars(ortho_dict):
+    """
+    Return the characters that are unique to each orthogrophy
+    """
+    unique_ortho_chars = {}
+    for key in ortho_dict:
+        unique_ortho_chars[key] = set(ortho_dict[key])
+        for key2 in ortho_dict:
+            if key != key2:
+                unique_ortho_chars[key].difference_update (set(ortho_dict[key2]))
+
+    return unique_ortho_chars
+
+def get_orthography(str, unique_ortho_chars):
+    """
+    Check string
+    """
+    for key, chars in unique_ortho_chars:
+        if 
+
+def main():
     parser = argparse.ArgumentParser(description='')
 
     parser.add_argument('string', type=str,
                     help='String to rewrite in various orthographies')
 
     args = parser.parse_args()
-    convert_dict = []
-    with open('../sounds/orthography.json', 'r') as f:
-        convert_dict = json.load(f)
 
-    #print (convert_dict)
+    with open('../sounds/orthography.json', 'r') as file:
+        ortho_dict = json.load(file)
 
-    for key in convert_dict:
-        convert_dict[key]['conv'] = ''
-        for ch in args.string:
-            if ch is not '':
-                pass
+    print (get_unique_chars(ortho_dict))
+
+    #print (ortho_dict)
+
+    # for key in ortho_dict:
+    #     ortho_dict[key]['conv'] = ''
+    #     for ch in args.string:
+    #         if ch is not '':
+    #             pass
             
+if __name__ == "__main__":
+    main()

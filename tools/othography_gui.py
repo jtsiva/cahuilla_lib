0fc025282e357789b95c652975ec7cbf55b0b181
Josh Siva <jtsiva@gmail.com>
2022-01-13 23:30:00 -0800

    simple buttons, label, text field working

--- tools/othography_gui.py
+++ tools/othography_gui.py
@@ -1,7 +1,37 @@
 #!/usr/bin/python3
 
-import tkinter #
-top = tkinter.Tk()
+from tkinter import *
+import json
+from functools import partial
 
-# Code to add widgets will go here...
-top.mainloop()
+def create_output_text (parent):
+    text_output = Text (parent)
+    text_output.pack(side=BOTTOM)
+    def button_press (char):
+        text_output.insert(END, char)
+
+    return button_press
+
+
+def create_orthography_buttons(parent, title, characters, button_action):
+    ortho_frame = Frame(parent)
+    ortho_frame.pack(side = TOP)
+    label = Label(ortho_frame, text = title)
+    label.pack (side = LEFT)
+
+    for char in characters:
+        btn = Button(ortho_frame, text = char, command = partial(button_action, char))
+        btn.pack(side = LEFT)
+
+def main():
+    root = Tk()
+    root.winfo_toplevel().title("Cahuilla Orthography")
+    output_text_update = create_output_text(root)
+    
+    create_orthography_buttons(root, 'aaa', ['a', 'b', 'c'], output_text_update)
+
+    # Code to add widgets will go here...
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()

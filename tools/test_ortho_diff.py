bcd456c321561902522970d8f80d421d4201c1c2
Josh Siva <jtsiva@gmail.com>
2022-01-20 08:33:06 -0800

    creating test for ortho_diff, not matching longer chars first, need json output api

---
+++ tools/test_ortho_diff.py
@@ -0,0 +1,6 @@
+import pytest
+
+def word_list_check():
+    saubel_list = ["waxachill", "sasang", "’ivillu’at"]
+    seiler_list = ["waxačil̃", "sasaŋ", "ʔivil̃uʔat"]
+    ipa_list = ["wɑxɑt͡ʃiʎ", "sɑsɑŋ", "ʔiviʎuʔɑt"]

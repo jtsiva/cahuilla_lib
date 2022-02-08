de03ccbb2a9e79ba3c559e8f0434396cf34d4182
Josh Siva <jtsiva@gmail.com>
2022-02-07 20:42:32 -0800

    finished implementing managed entry with tests

--- tools/util/managed_entry.py
+++ tools/util/managed_entry.py
@@ -6,16 +6,14 @@ class ManagedEntry ():
     additional keys upon creation
     """
 
-
     #protected
     _entry = None #python dictionary containing related fields
     
-
     def __init__ (self, entry, editable):
         """
         Initializer entry data and editable status
 
-        entry - the starting dictionary
+        entry - the starting dictionary--could be a schema
         editable - the status True/False of whether entry is editable
         """
         self._entry = entry
@@ -23,13 +21,20 @@ class ManagedEntry ():
 
     def __getitem__ (self, key):
         """
-        Return item from _entry. Can throw key error
+        Return item from _entry.
         key - key from which to get value
         """
         return self._entry[key]
 
-    def __setitem__ (self, key, val):
+    def __setitem__ (self, key, value):
+        """
+        Set an item if the entry is set to editable and the key already exists. Raises KeyError if conditions not met
 
-    
+        key - the key to update
+        value - the new value for the key
+        """
 
-
+        if self.editable and key in self._entry:
+            self._entry[key] = value
+        else:
+            raise KeyError("Key is not editable or doesn't exist")
--- tools/util/test_managed_entry.py
+++ tools/util/test_managed_entry.py
@@ -1,7 +1,33 @@
 #!/bin/python3
 
 import pytest
-import managed_entry
+from managed_entry import ManagedEntry
 
-def test_editable():
-    pass
+def test_edit_editable():
+    test_dict = {"a" : 1}
+    entry = ManagedEntry (test_dict, True)
+
+    entry["a"] = 2
+    assert (entry["a"] == 2)
+
+def test_edit_non_editable():
+    test_dict = {"a" : 1}
+    entry = ManagedEntry (test_dict, False)
+
+    try:
+        entry["a"] = 2
+    except KeyError:
+        assert (True)
+    else:
+        assert(False)
+
+def test_edit_non_key():
+    test_dict = {"a" : 1}
+    entry = ManagedEntry (test_dict, False)
+
+    try:
+        entry["b"] = 2
+    except KeyError:
+        assert (True)
+    else:
+        assert(False)

557f5c715214087fe529f1f620c7951681c3a5b4
Josh Siva <jtsiva@gmail.com>
2022-02-18 15:28:14 -0800

    added skeleton of dictionary

--- tools/dictionary/dictionary.py
+++ tools/dictionary/dictionary.py
@@ -15,7 +15,41 @@ class Dictionary():
         for json_entry in word_list:
             self._entries.append(ManagedEntry(json_entry, False))
 
-    
+    def lookup(self, search_term):
+        """
+        Use the search term to return a list of words that best
+        match the term
+        """
+        return_words = None
+
+        return return_words
+
+    def edit (self, field, value):
+        """
+        Flag or suggest edit
+        """
+        pass
+
+    def get_usage (self, word):
+        """
+        Return a list of examples where the word is used
+        """
+        pass
+
+    def add(self, entry_data):
+        """
+        Add a word to the dictionary. All data expected by schema
+        should be provided
+        """
+        pass
+
+    def delete(self, entry_id):
+        """
+        Remove an entry by ID
+        """
+        pass
+
+
 
 
 

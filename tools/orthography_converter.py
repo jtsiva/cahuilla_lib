348309eaea7d89964deec59adb5d92b6bc61003f
Josh Siva <jtsiva@gmail.com>
2022-04-05 17:55:36 -0700

    using bootstrap templates. Simple page created with functionless input field. need to reorg

--- tools/orthography_converter.py
+++ tools/orthography_converter.py
@@ -1,10 +1,11 @@
 #!/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '2647c09074614974e9a936a37e2265ec'
 
 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')

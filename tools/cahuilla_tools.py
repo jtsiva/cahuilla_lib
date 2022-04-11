9482f6ef75d4a69a2e5a6eb5db060943ceb6a7bd
Josh Siva <jtsiva@gmail.com>
2022-04-10 20:52:46 -0700

    added search with simple table for results

--- tools/cahuilla_tools.py
+++ tools/cahuilla_tools.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request
 from orthography.orthography import Orthography
+from dictionary.dictionary import Dictionary
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '2647c09074614974e9a936a37e2265ec'
@@ -23,11 +24,11 @@ def orthography():
 
 @app.route('/dictionary', methods=['GET', 'POST'])
 def dictionary():
-    
-    # if request.method == 'POST':
-    #     text = ortho.convert_orthography(request.form['input'].strip())
-    #     return render_template('orthography.html', text=text)
-    # elif request.method == 'GET':
-    #     return render_template('orthography.html', text=text)
+    cahuilla_dict = Dictionary("dictionary/schema_v2.json", "../words/dict.json")
 
-    return render_template('dictionary.html')
+    cahuilla_dict.load()
+    results = []
+    if request.method == 'POST':
+        results = cahuilla_dict.lookup(request.form['input'].strip())
+
+    return render_template('dictionary.html', results=results)

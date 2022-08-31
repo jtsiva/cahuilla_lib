134d150026ffee418bbe38e9782f9e4457508ac3
Josh Siva <jtsiva@gmail.com>
2022-08-30 21:25:07 -0700

    found better replacement scheme, handling accents, IPA incomplete

--- tools/orthography/test_ortho_diff.py
+++ tools/orthography/test_ortho_diff.py
@@ -2,15 +2,16 @@ import pytest
 from orthography import Orthography
 
 def test_word_list_check():
-    ortho = Orthography ('../sounds/orthography.json')
+    ortho = Orthography ('../../sounds/orthography.json')
     test_words = {"saubel_chemivillu" : ["wáxachill", "sásang", "'ívillu'at"],
     "seiler_dictionary" : ["wáxačil̃", "sásaŋ", "ʔívil̃uʔat"],
-    "IPA" : ["wɑxɑt͡ʃiʎ", "sɑsɑŋ", "ʔíviʎuʔɑt"]}
+    "IPA" : ["wɑxɒt͡ʃɨʎ", "sɑsɒŋ", "ʔivɨʎuʔɒt"]}
 
     #iterate over all the lists and make sure that each pair
     #and direction is correct
     for ortho_name in test_words:
         for word1 in test_words[ortho_name]:
             convert_output = ortho.convert_orthography(word1)
+            print (convert_output)
             for key, word2 in convert_output["converted"].items():
                 assert (word2 in test_words[key])
